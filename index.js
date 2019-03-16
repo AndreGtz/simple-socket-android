38d20e670a3cdeac0b76a1450b5742c070edcb2e
andregtz <colpac64@gmail.com>
2019-03-15 19:25:10 -0600

    origin

---
+++ index.js
@@ -0,0 +1,51 @@
+const net = require('net');
+const request = require('request-promise');
+
+const { port, serverurl } = process.env;
+
+const serveCommands = net.createServer((socket) => {
+  let token;
+  socket.setEncoding('utf8');
+
+  socket.on('data', (data) => {
+    console.info(data);
+    if (/^#.*/.test(data)) {
+      const [imei, latitud, longitud, velocidad] = data.substring(1).split(',');
+      request({
+        method: 'POST',
+        uri: serverurl,
+        body: {
+          imei,
+          latitud,
+          longitud,
+          velocidad,
+        },
+        json: true,
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then(() => {
+          console.info('saved');
+        })
+        .catch((err) => {
+          console.err(err);
+        });
+    } else {
+      token = data;
+    }
+  });
+
+  socket.on('end', () => {
+    console.info('ended conection');
+  });
+
+  socket.on('error', (e) => {
+    console.error(e);
+  });
+});
+
+serveCommands.on('error', (error) => { console.info(error); });
+
+serveCommands.listen(port);
+console.info(`Listening at ${port}`);
